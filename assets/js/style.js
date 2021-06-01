f7ed78f13a4c1ae95641be7bf7bb11ebced9461a
Your Name <nickmihaly@gmail.com>
2021-05-31 22:24:48 -0700

    add spotify key

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,3 +1,23 @@
-$(document).ready(function(){
-    $('select').formSelect();
-});
+var APIController = (function() {
+    
+    var clientId = '1ce2babdd7f44a1d86098faf54333881';
+    var clientSecret = 'd5613cd75a52445db1f1fc900e62bf73';
+
+    // private methods
+    var _getToken = async () => {
+
+        var result = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded', 
+                'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
+            },
+            body: 'grant_type=client_credentials'
+        });
+
+        var data = await result.json();
+        return data.access_token;
+    }
+})();
+    
+    

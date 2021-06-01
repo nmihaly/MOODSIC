022bb90d0a2e7446b53578a2e6baa631c5fda625
Your Name <nickmihaly@gmail.com>
2021-05-31 23:33:59 -0700

    fetch genre/track/playlist form API

--- assets/js/style.js
+++ assets/js/style.js
@@ -18,6 +18,73 @@ var APIController = (function() {
         var data = await result.json();
         return data.access_token;
     }
+    
+    var _getGenres = async (token) => {
+
+        var result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        var data = await result.json();
+        return data.categories.items;
+    }
+
+    var _getPlaylistByGenre = async (token, genreId) => {
+
+        var limit = 3;
+        
+        var result = await fetch(`https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        var data = await result.json();
+        return data.playlists.items;
+    }
+
+    var _getTracks = async (token, tracksEndPoint) => {
+
+        var limit = 3;
+
+        var result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        var data = await result.json();
+        return data.items;
+    }
+
+    var _getTrack = async (token, trackEndPoint) => {
+
+        var result = await fetch(`${trackEndPoint}`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        var data = await result.json();
+        return data;
+    }
+
+    return {
+        getToken() {
+            return _getToken();
+        },
+        getGenres(token) {
+            return _getGenres(token);
+        },
+        getPlaylistByGenre(token, genreId) {
+            return _getPlaylistByGenre(token, genreId);
+        },
+        getTracks(token, tracksEndPoint) {
+            return _getTracks(token, tracksEndPoint);
+        },
+        getTrack(token, trackEndPoint) {
+            return _getTrack(token, trackEndPoint);
+        }
+    }
 })();
-    
-    
+
+
+
